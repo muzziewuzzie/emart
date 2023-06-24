9fd48a9052f4151a63814c505eabe362e7129549
Muzakkir Syed <musavvirmuzakkir@gmail.com>
2023-06-24 13:18:47 +0800

    initialised project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environment } from './../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCPx2gZ90B5mcAMmzVvXNDO8QfqNkSbcuY',
+    authDomain: 'mcube-emart.firebaseapp.com',
+    projectId: 'mcube-emart',
+    storageBucket: 'mcube-emart.appspot.com',
+    messagingSenderId: '79005491545',
+    appId: '1:79005491545:web:f9cf7f15c2e4d5eeebec48',
+    measurementId: 'G-1VQVLZPGW8',
+  },
+};
